10c039630cdb9d962a4ccfbbab1dbee7551f9090
Siravet <sittikorn146@gmail.com>
2025-07-21 13:07:49 +0700

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,14 +1,31 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'authen.dart';
 import '---Translate---/locale_manager.dart';
 
 
 final themeModeNotifier = ValueNotifier<ThemeMode>(ThemeMode.light);
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _loadInitialSettings();
   runApp(MyApp());
+}
+
+Future<void> _loadInitialSettings() async {
+  final prefs = await SharedPreferences.getInstance();
+  
+  // Load dark mode setting
+  final isDark = prefs.getBool('dark_mode') ?? false;
+  themeModeNotifier.value = isDark ? ThemeMode.dark : ThemeMode.light;
+  
+  // Load language setting
+  final langCode = prefs.getString('language_code') ?? 'th';
+  localeManager.setLocale(langCode == 'th' 
+    ? const Locale('th', 'TH') 
+    : const Locale('en', 'US'));
 }
 
 class MyApp extends StatelessWidget {
